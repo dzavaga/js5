38492b2a3f9fe205da3a9abe862d80c8a649ce38
cipher <mishaofis86@gmail.com>
2023-04-27 17:29:35 +0300

    code

--- main.js
+++ main.js
@@ -57,3 +57,116 @@ let mentor = {
 console.log(propsCount(mentor)); // 3*/
 //3
 
+/*class Person {
+  constructor(name, surname) {
+    this.name = name;
+    this.surname = surname;
+  }
+
+  showFullName() {
+    console.log(this.surname + " " + this.name);
+  }
+}
+
+class Student extends Person {
+  constructor(name, surname, year) {
+    super(name, surname);
+    this.year = year;
+  }
+
+  showFullName(middleName) {
+    console.log(this.surname + " " + this.name + " " + middleName);
+  }
+
+  showCourse() {
+    const currentYear = new Date().getFullYear();
+    const course = currentYear - this.year;
+    return Math.max(1, Math.min(6, course));
+  }
+}
+
+const stud1 = new Student("Petro", "Petrenko", 2019);
+stud1.showFullName("Petrovych"); // Petrenko Petro Petrovych
+console.log("Current course: " + stud1.showCourse()); // Current course: 4
+*/
+//4
+//A
+/*class SimpleMarker {
+  constructor(color, inkAmount) {
+    this.color = color;
+    this.inkAmount = inkAmount;
+  }
+
+  print(text) {
+    let printedText = '';
+    for (let i = 0; i < text.length; i++) {
+      if (this.inkAmount >= 0.5) {
+        printedText += text[i];
+        if (text[i] !== ' ') {
+          this.inkAmount -= 0.5;
+        }
+      } else {
+        break;
+      }
+    }
+    console.log('%c' + printedText, `color: ${this.color}`);
+  }
+}
+//B
+class RefillableMarker extends SimpleMarker {
+  constructor(color, inkCapacity) {
+    super(color, 100);
+    this.inkCapacity = inkCapacity;
+  }
+
+  refill() {
+    this.inkAmount = this.inkCapacity;
+  }
+}*/
+//5
+class Worker {
+  constructor(fullName, dayRate, workingDays) {
+    this.fullName = fullName;
+    this.dayRate = dayRate;
+    this.workingDays = workingDays;
+    this._experience = 1.2;
+  }
+
+  showSalary() {
+    let salary = this.dayRate * this.workingDays;
+    console.log(`${this.fullName} salary: ${salary}`);
+    return salary;
+  }
+
+  showSalaryWithExperience() {
+    let salary = this.dayRate * this.workingDays * this._experience;
+    console.log(`${this.fullName} salary: ${salary}`);
+    return salary;
+  }
+
+  get showExp() {
+    return this._experience;
+  }
+
+  set setExp(value) {
+    this._experience = value;
+  }
+}
+
+let workers = [
+  new Worker("John Johnson", 20, 23),
+  new Worker("Tom Tomson", 48, 22),
+  new Worker("Andy Ander", 29, 23)
+];
+
+workers.forEach(worker => {
+  console.log(worker.fullName);
+  worker.showSalary();
+  console.log("New experience: " + worker.showExp);
+  worker.showSalaryWithExperience();
+});
+
+workers.sort((a, b) => a.showSalaryWithExperience() - b.showSalaryWithExperience());
+
+console.log("Sorted salary:");
+workers.forEach(worker => console.log(`${worker.fullName}: ${worker.showSalaryWithExperience()}`));
